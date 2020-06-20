843d0d711a49fdbca11fb093812631b0ac0e39c1
alifahnurhana <alifahnur2609@gmail.com>
2020-06-20 23:31:35 +0800

    test

--- src/Ifa.java
+++ src/Ifa.java
@@ -1,6 +1,11 @@
+import learnOOP.Fahh;
+import learnOOP.Ferarri;
+
 public class Ifa {
     public static void main(String[] args) {
         System.out.println("aasdasadasd");
-        
+        Fahh obfahh = new Fahh();
+        obfahh.run();
     }
+
 }
---
+++ src/learnOOP/Fahh.java
@@ -0,0 +1,13 @@
+package learnOOP;
+
+public class Fahh extends Ferarri{
+    @Override
+    public void race() {
+        super.race();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
